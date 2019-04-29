abe0b7f519e9a2b20218aa5b5a85f844512444db
shozonu <a14n.liu+github@gmail.com>
2019-04-28 21:26:18 -0700

    Added flex sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Sidebar() {
+  return (
+    <div className="Side">
+        <div>Sidebar</div>
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Sidebar from './Sidebar';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Sidebar />, document.getElementById('side'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
