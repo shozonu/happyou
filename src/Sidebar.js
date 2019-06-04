6cbc30fac67844e6b71718246e9ff229c0218223
shozonu <a14n.liu+github@gmail.com>
2019-06-04 16:22:08 -0700

    Refactored Sidebar into a React component.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
-
-function Sidebar() {
-    let title = [];
-    for(let i = 0; i < 4; i++) {
-        let e = GenerateEntryDiv("Sidebar Entry " + i);
-        title.push(e);
+class Sidebar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {entries: []};
+        //add a command object or function delegate to each entries?
     }
-
-    return (
-        <div className="Side">
-            <div className ="side-container">
-                {title}
+    componentDidMount() {
+        let entries = [];
+        for(let i = 0; i < 4; i++) {
+            let name = "Sidebar Entry " + i;
+            let e = GenerateEntryDiv(name);
+            entries.push(e);
+        }
+        this.setState({entries: entries});
+    }
+    render() {
+        let entries = [];
+        entries.push({name: "test name"});
+        return (
+            <div className="Side">
+                <div className ="side-container">
+                    {this.state.entries}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 function GenerateEntryDiv(title) {
