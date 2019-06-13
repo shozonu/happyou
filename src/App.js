115d932f4779b8c414dd4bc3dc3374a7e0008d2d
shozonu <a14n.liu+github@gmail.com>
2019-06-12 17:58:38 -0700

    Testing displaying response from http://www.dnd5eapi.co

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ class App extends React.Component {
         super(props);
         this.handleChangeApp = this.handleChangeApp.bind(this);
         this.getApp = this.getApp.bind(this);
+        this.getAPIResponse = this.getAPIResponse.bind(this);
         if(props.app != null) {
-            this.state = {app: props.app};
+            this.state = {
+                app: props.app,
+                data: {}
+            };
         }
         else {
-            this.state = {app: "None. Click one of the sidebar entries!"};
+            this.state = {
+                app: "None. Click one of the sidebar entries!",
+                data: {}
+            };
         }
     }
     componentDidMount() {
         this.setState({appObj: this.getApp("default")});
         this.element.addEventListener("changeapp", this.handleChangeApp);
+        this.getAPIResponse("/spells/1/");
     }
     componentWillDismount() {
         this.element.removeEventListener("changeapp");
@@ -32,12 +40,14 @@ class App extends React.Component {
     }
     getApp(name) {
         if(name === "Spells") {
-            let content = getAPIResponse("/spells/1/");
             return (
                 <div className="App" ref={elem => this.element = elem}>
                     <header className="App-header">
                         <p>
-                            {content}
+                            Spell: {this.state.data.name}<br/>
+                            <code>
+                                Description: {this.state.data.desc[0]}<br/>
+                            </code>
                         </p>
                     </header>
                 </div>
@@ -56,18 +66,21 @@ class App extends React.Component {
             );
         }
     }
-}
-
-function getAPIResponse(s) {
-    let xhttp = new XMLHttpRequest();
-    let url = "http://www.dnd5eapi.co/api" + s;
-    xhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            return xhttp.responseText;
-        }
+    getAPIResponse(s) {
+        let scope = this;
+        let xhttp = new XMLHttpRequest();
+        let url = "http://www.dnd5eapi.co/api" + s;
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                let obj = JSON.parse(xhttp.responseText);
+                console.log(obj);
+                scope.setState({data: obj});
+                return obj;
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
     }
-    xhttp.open("GET", url, false);
-    xhttp.send();
 }
 
 export default App;
