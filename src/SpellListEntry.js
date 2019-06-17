d4bcfb421775133b90bbec45a5dde631573e950c
shozonu <a14n.liu+github@gmail.com>
2019-06-17 16:40:42 -0700

    missing semicolon

--- src/SpellListEntry.js
+++ src/SpellListEntry.js
@@ -4,5 +4,21 @@ import './App.css';
 class SpellListEntry extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name: props.name,
+            url: props.url
+        };
+    }
+    componentDidMount() {
+        //
+    }
+    render() {
+        let e = <div>{this.state.name}</div>;
+        return e;
+    }
+    handleClick() {
+        //
     }
 }
+
+export default SpellListEntry;
