5aa18aa552d3c8982ff0ded919cc6c70b9a33d3d
shozonu <a14n.liu+github@gmail.com>
2019-06-23 16:06:29 -0700

    Display entries by page and max entries. Check for state before render.

--- src/SpellList.js
+++ src/SpellList.js
@@ -8,46 +8,60 @@ class SpellList extends React.Component {
         this.fetchContent = this.fetchContent.bind(this);
         this.state = {
             count: 0,
-            results: []
+            results: [],
+            pageNumber: 1,
+            pageMax: 16,
+            retrieved: false
         };
         this.fetchContent("http://www.dnd5eapi.co/api/spells/");
     }
     render() {
-        let list = [];
-        let max = this.state.count;
-        if(max > 5) {
-            max = 5;
+        if(this.state.retrieved === true) {
+            // Display SpellListEntries from entire list
+            // depending on current page and maximum entries per page
+            let list = [];
+            for(let i = (this.state.pageMax * (this.state.pageNumber - 1));
+            i < (this.state.pageNumber * this.state.pageMax) - 1; i++) {
+                let o = <SpellListEntry
+                    key={i}
+                    name={this.state.results[i].name}
+                    url={this.state.results[i].url}
+                />;
+                list.push(o);
+            }
+            console.log("(Re)rendering SpellList.");
+            console.log("displaying " + this.state.pageMax);
+            return(
+                <div className="App-content">
+                    <div>Results: {this.state.results.count}</div>
+                    {list}
+                </div>
+            );
         }
-        for(let i = 0; i < max; i = i + 1) {
-            let o = <SpellListEntry
-                key={i}
-                name={this.state.results[i].name}
-                url={this.state.results[i].url}
-            />;
-            list.push(o);
+        else {
+            return(
+                <div className="App-content">
+                    <div>Loading</div>
+                </div>
+            );
         }
-        console.log("(Re)rendering SpellList.");
-        console.log("displaying " + max);
-        return(
-            <div className="App-content">
-                <div>Results: {this.state.results.count}</div>
-                {list}
-            </div>
-        );
     }
     async fetchContent(url) {
-        let response = await fetch(url).then(result => {
-            return result.json();
-        });
-        let list = [];
-        for(let i = 0; i < response.results.length; i++) {
-            let s = String(response.results[i]);
-            list.push(s.replace(/â€™/g, "'"));
+        if(this.state.retrieved === false) {
+            let response = await fetch(url).then(result => {
+                return result.json();
+            });
+            let list = [];
+            for(let i = 0; i < response.results.length; i++) {
+                let s = String(response.results[i]);
+                list.push(s.replace(/â€™/g, "'"));
+            }
+            this.setState({
+                count: response.count,
+                results: response.results,
+                retrieved: true
+            });
         }
-        this.setState({
-            count: response.count,
-            results: response.results
-        });
     }
 }
 
