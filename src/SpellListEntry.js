723529bcbc0f332299a8de991a0dc47dec4e5a73
shozonu <a14n.liu+github@gmail.com>
2019-07-14 22:24:40 -0700

    Moved onClick function into own component function.

--- src/SpellListEntry.js
+++ src/SpellListEntry.js
@@ -4,30 +4,30 @@ import './App.css';
 class SpellListEntry extends React.Component {
     constructor(props) {
         super(props);
+        this.showSpellModal = this.showSpellModal.bind(this);
         this.state = {
             name: props.name,
             url: props.url
         };
     }
     render() {
-        const context = this;
-        let f = function() {
-            document.getElementsByClassName("App")[0]
-            .dispatchEvent(new CustomEvent("showSpellModal", {
-                bubbles: false,
-                detail: {
-                    name: context.state.name,
-                    changeTo: "spellModal",
-                    data: {
-                        spell: {
-                            url: context.state.url
-                        }
+        let e = <div onClick={this.showSpellModal} className="SpellList-entry">{this.state.name}</div>;
+        return e;
+    }
+    showSpellModal() {
+        document.getElementsByClassName("App")[0]
+        .dispatchEvent(new CustomEvent("showSpellModal", {
+            bubbles: false,
+            detail: {
+                name: this.state.name,
+                changeTo: "spellModal",
+                data: {
+                    spell: {
+                        url: this.state.url
                     }
                 }
-            }));
-        };
-        let e = <div onClick={f} className="SpellList-entry">{this.state.name}</div>;
-        return e;
+            }
+        }));
     }
 }
 
